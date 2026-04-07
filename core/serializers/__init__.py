42cb3d6bfc091d703319d68075b427ad8a87ddc3
helenasf8 <helenadasilvafortunato.if@gmail.com>
2026-04-07 09:24:38 -0300

    feat: criação de múltiplos serializadores para livro

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,4 +2,5 @@ from .autor import AutorSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .livro import LivroSerializer
+from .livro import LivroRetrieveSerializer, LivroListSerializer ,LivroSerializer
 from .user import UserRegistrationSerializer, UserSerializer
