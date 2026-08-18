1ca42b80b503cf4c709507b6116d35d9f4f28f75
helenasf8 <helenadasilvafortunato.if@gmail.com>
2026-08-18 16:01:01 -0300

    feat: criação de um endpoint para atualizar compras

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -45,10 +45,10 @@ class CompraCreateUpdateSerializer(ModelSerializer):
         fields = ('id', 'usuario', 'itens')
 
     @transaction.atomic
-    def create(self, validated_data):
-        itens_data = validated_data.pop('itens')
-        compra = Compra.objects.create(**validated_data)
-        for item_data in itens_data:
-            ItensCompra.objects.create(compra=compra, **item_data)
-        compra.save()
-        return compra
+    def update(self, compra, validated_data):
+        itens_data = validated_data.pop('itens', None)
+        if itens_data is not None:
+            compra.itens.all().delete()
+            for item_data in itens_data:
+                ItensCompra.objects.create(compra=compra, **item_data)
+        return super().update(compra, validated_data)
